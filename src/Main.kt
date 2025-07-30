8de5120c93efdd68e5d009eeca6dc94775eb6f01
Luis <langelgirona@gmail.com>
2025-07-29 22:30:45 -0600

    add main function to demonstrate cooking methods for Food subclasses

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun Food.discountedPrice(descuento: Double): Double {
     return this.price * (100 - descuento) / 100
 }
+
+fun main() {
+    val hamburguesa = Burguer()
+    val pizza = Pizza()
+    val helado = IceCream()
+    val jugo = Juice()
+
+    println(hamburguesa.cook())
+    println(pizza.cook())
+    println(helado.cook())
+    println(jugo.cook())
+}
